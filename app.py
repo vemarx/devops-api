189513974eb1c37a2585c3a51ae3883d3333ac37
vemarx <vemarx.dev@gmail.com>
2026-02-27 17:54:11 -0300

    feat: adiciona rota de status

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return {"mensagem": "API funcionando corretamente"}
 
+@app.route("/status")
+def status():
+    return {"status": "API online"}
+
 if __name__ == "__main__":
     app.run(debug=True)
